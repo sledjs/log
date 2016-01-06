30adedfbbadfdb9192e5d9233eb17d5c5e6544bd
Damian <carftjr@gmail.com>
2016-01-06 15:56:29 +0100

    give up es6 modules for now

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-export default function log(...logs) {
-  console.log(`[${new Date().toLocaleTimeString()}]`, ...logs);
-}
+'use strict';
+
+module.exports = function log(...logs) {
+  console.log(`[${new Date().toLocaleTimeString()}]`, ...processId(logs));
+};
+
+function processId(logs) {
+  if (typeof logs[0] == 'object') {
+
+    if (logs[0].id)
+      logs[0] = `[${logs[0].id}]`;
+    else
+      logs.shift();
+  }
+
+  return logs;
+};
