4a3ea0adf58558d88b3433b7beeae02f9219e815
rascada <carftjr@gmail.com>
2016-02-05 02:55:28 +0100

    logs now can have names

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ module.exports = function log(...logs) {
 
 function processId(logs) {
   if (typeof logs[0] == 'object') {
+    let log = logs[0];
 
-    if (logs[0].id)
-      logs[0] = `[${logs[0].id}]`;
-    else
-      logs.shift();
+    logs[0] = `[${log.id}]`;
+
+    if (log.name)
+      logs[0] += ` [${log.name}]`;
   }
 
   return logs;
